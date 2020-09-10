029236f0af027472b77edbc65dba9fc28fcea78e
stanislav-kelemen <oxxx1stas@gmail.com>
2020-09-10 11:55:53 +0300

    Updates

---
+++ precache-manifest.c2f5a499b995cea41d6a5b78ce4b0ea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ae122904b065cc04cbd05e1a8d6d22",
+    "url": "/swapi-react/index.html"
+  },
+  {
+    "revision": "5839dd6db24610db1fca",
+    "url": "/swapi-react/static/css/main.54a87bbb.chunk.css"
+  },
+  {
+    "revision": "dd2def31de3c158d2ada",
+    "url": "/swapi-react/static/js/2.5215210c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/swapi-react/static/js/2.5215210c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5839dd6db24610db1fca",
+    "url": "/swapi-react/static/js/main.0f168c2b.chunk.js"
+  },
+  {
+    "revision": "9f3c9d5e34374e28aef8",
+    "url": "/swapi-react/static/js/runtime-main.7119f7fd.js"
+  }
+]);
